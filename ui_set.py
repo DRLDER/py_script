08297626e032e87c6f8de0e87d54beb62380dda2
小馬 HI <836444147@qq.com>
2021-01-30 22:02:11 +0800

    py excel script

--- ui_set.py
+++ ui_set.py
@@ -1,3 +1,6 @@
+import webbrowser
+
+import requests
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 
@@ -34,20 +37,20 @@ class Ui_Form(object):
         self.ExecuteBtn.setGeometry(QtCore.QRect(314, 100, 81, 23))
         self.ExecuteBtn.setObjectName("ExecuteBtn")
         self.showLabel = QtWidgets.QLabel(self.tab)
-        self.showLabel.setGeometry(QtCore.QRect(40, 140, 121, 21))
+        self.showLabel.setGeometry(QtCore.QRect(30, 140, 131, 21))
         self.showLabel.setObjectName("showLabel")
-        self.widget = QtWidgets.QWidget(self.tab)
-        self.widget.setGeometry(QtCore.QRect(40, 20, 351, 33))
-        self.widget.setObjectName("widget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.layoutWidget = QtWidgets.QWidget(self.tab)
+        self.layoutWidget.setGeometry(QtCore.QRect(40, 20, 351, 33))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.layoutWidget)
         self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setSpacing(6)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label = QtWidgets.QLabel(self.widget)
+        self.label = QtWidgets.QLabel(self.layoutWidget)
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
-        self.pathEdit = QtWidgets.QLineEdit(self.widget)
+        self.pathEdit = QtWidgets.QLineEdit(self.layoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(12)
         sizePolicy.setVerticalStretch(0)
@@ -55,7 +58,7 @@ class Ui_Form(object):
         self.pathEdit.setSizePolicy(sizePolicy)
         self.pathEdit.setObjectName("pathEdit")
         self.horizontalLayout.addWidget(self.pathEdit)
-        self.openFileBtn = QtWidgets.QPushButton(self.widget)
+        self.openFileBtn = QtWidgets.QPushButton(self.layoutWidget)
         self.openFileBtn.setEnabled(True)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(2)
@@ -69,12 +72,32 @@ class Ui_Form(object):
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
+        self.label_4 = QtWidgets.QLabel(self.tab_2)
+        self.label_4.setGeometry(QtCore.QRect(63, 30, 31, 20))
+        self.label_4.setObjectName("label_4")
+        self.urledit = QtWidgets.QLineEdit(self.tab_2)
+        self.urledit.setGeometry(QtCore.QRect(90, 30, 291, 20))
+        self.urledit.setObjectName("urledit")
+        self.TakeBtn = QtWidgets.QPushButton(self.tab_2)
+        self.TakeBtn.setGeometry(QtCore.QRect(300, 60, 81, 23))
+        self.TakeBtn.setObjectName("TakeBtn")
+        self.label_5 = QtWidgets.QLabel(self.tab_2)
+        self.label_5.setGeometry(QtCore.QRect(30, 100, 61, 20))
+        self.label_5.setObjectName("label_5")
+        self.photourledit = QtWidgets.QLineEdit(self.tab_2)
+        self.photourledit.setGeometry(QtCore.QRect(90, 100, 291, 20))
+        self.photourledit.setObjectName("photourledit")
+        self.openphotourlbtn = QtWidgets.QPushButton(self.tab_2)
+        self.openphotourlbtn.setGeometry(QtCore.QRect(300, 130, 81, 23))
+        self.openphotourlbtn.setObjectName("openphotourlbtn")
         self.tabWidget.addTab(self.tab_2, "")
+        self.tab_3 = QtWidgets.QWidget()
+        self.tab_3.setObjectName("tab_3")
+        self.tabWidget.addTab(self.tab_3, "")
 
         self.retranslateUi(Form)
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(Form)
-        self.tickEdit.setText("500")
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
@@ -87,7 +110,13 @@ class Ui_Form(object):
         self.label.setText(_translate("Form", "文件选中："))
         self.openFileBtn.setText(_translate("Form", "..."))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Form", "青年大学习中奖人筛选"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("Form", "Building..."))
+        self.label_4.setText(_translate("Form", "URL:"))
+        self.TakeBtn.setText(_translate("Form", "提取"))
+        self.label_5.setText(_translate("Form", "PhotoURL:"))
+        self.photourledit.setText(_translate("Form", "Null"))
+        self.openphotourlbtn.setText(_translate("Form", "打开图片链接"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("Form", "公众号封面提取"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("Form", "Building..."))
 
     def actionList(self):
         def openfilebtn_action():
@@ -107,8 +136,38 @@ class Ui_Form(object):
             else:
                 The_main_path = self.pathEdit.text()
                 memberTick = self.tickEdit.text()
-                Excel_action(FilePathLine,FilePathNameLine,int(self.tickEdit.text()),self.newFileNameEdit.text())
+                Excel_action(FilePathLine, FilePathNameLine, int(self.tickEdit.text()), self.newFileNameEdit.text())
                 QMessageBox.about(None, "Warning", "action!" + " " + The_main_path)
+
+
+
+        def take_action():
+            if (self.urledit.text() == ""):
+                QMessageBox.about(None, "Warning", "The Url is Null!")
+            else:
+                weburl = self.urledit.text()
+                res = requests.get(weburl)
+                res.encoding = 'utf-8'
+                htmltext = res.text
+                firstplace = htmltext.find("var msg_cdn_url")
+                if (firstplace == -1):
+                    QMessageBox.about(None, "Warning", "此公众号的封面被隐藏，或非公众号图文链接！")
+                else:
+                    endplace = htmltext.find(";", firstplace)
+                    firstnip = htmltext.find('"', firstplace)
+                    nextnip = htmltext.find('"', firstnip + 1)
+                    photourl = htmltext[firstnip + 1:nextnip]
+                    self.photourledit.setText(photourl)
+                    global photourltext
+                    photourltext = photourl
+
+        def openphotourl_action():
+            if (photourltext == "Null" or photourltext == ""):
+                QMessageBox.about(None, "Warning", "打开封面链接失败！")
+            else:
+                webbrowser.open_new_tab(photourltext)
 
         self.openFileBtn.clicked.connect(openfilebtn_action)
         self.ExecuteBtn.clicked.connect(execute_action)
+        self.TakeBtn.clicked.connect(take_action)
+        self.openphotourlbtn.clicked.connect(openphotourl_action)
